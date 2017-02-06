0c3b812fbbf6df59e26aa9db7f38d4488e4d283a
muming <muming90@foxmail.com>
2017-02-06 11:07:07 +0800

    update

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 #!/usr/bin/env node
 
 const program = require('commander');
-const path = require('path');
-const fs = require('fs');
-const rm = require("rimraf");
-const download = require('download-git-repo');
+const init = require('./lib/init.js');
 const package = require('./package.json');
 
 program.version(package.version);
@@ -13,32 +10,9 @@ program.usage('init [project-name]');
 
 program.command('init [project-name]')
     .description('generate a new project')
+    .alias('i')
     .action(function(name) {
-        if(!name) return console.log('--- no input project name ---');
-
-        // 目标路径
-        const dest = path.resolve('./', name);
-        console.log(dest);
-
-        // 脚手架仓库地址
-        const repo = 'rabbitmu/mutu-cli-template';
-
-        // 判断目标路径是否已经存在
-        fs.exists(dest, function(isExist) {
-            if(isExist) return console.log('--- ' + name + ' is an existing directory ---');
-
-            fs.mkdir(dest, function(error) {
-                if(error) return console.log(error);
-
-                download(repo, dest, function(error) {
-                    if(error) {
-                        rm(dest, function(error) { if(error) console.log(error); });
-                        return console.log(error);
-                    }
-                    console.log('--- download success ---');
-                });
-            });
-        });
+        init(name);
     });
 
 program.parse(process.argv);
